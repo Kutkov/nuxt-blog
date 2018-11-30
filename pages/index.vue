2707160312ef728158338820c0da45e43e10a0ea
Max <max.k194@gmail.com>
2018-11-30 19:13:26 +0300

    28 add bg

--- pages/index.vue
+++ pages/index.vue
@@ -4,54 +4,52 @@
       <h1>Get the latest tech news!</h1>
     </section>
     <section class="featured-posts">
-      <nuxt-link :to="'/posts/' + 1">
-        <article class="post-preview">
-          <div class="post-thumbnail" style="background-image: url('https://icdn.lenta.ru/images/2018/09/21/15/20180921152446974/pic_dff6499e5b87f34e7a8e4bcb50f8fb10.jpg');"></div>
-          <div class="post-content">
-            <h1>Post Title</h1>
-            <p>Preview text</p>
-          </div>
-        </article>
-      </nuxt-link>
-      <nuxt-link :to="'/posts/' + 2">
-        <article class="post-preview">
-          <div class="post-thumbnail" style="background-image: url('https://kareliyanews.ru/wp-content/uploads/2018/08/neva-city.jpg');"></div>
-          <div class="post-content">
-            <h1>Post Title 2</h1>
-            <p>Preview text 2</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview 
+        id="1"
+        thumbnail="https://icdn.lenta.ru/images/2018/09/21/15/20180921152446974/pic_dff6499e5b87f34e7a8e4bcb50f8fb10.jpg"
+        title="Hello there"
+        previewText="This my hello"
+      />
+      <PostPreview 
+        id="2"
+        thumbnail="http://www.vsemirnyjbank.org/content/dam/photos/780x439/2017/jun-1/ru-city-780.jpg"
+        title="Hello there2"
+        previewText="This my hello3"
+      />
+      <PostPreview 
+        id="3"
+        thumbnail="https://forbes.kz/img/articles/99661f12e332c908c2a3a754d3f4942b-big.jpg"
+        title="Hello there3"
+        previewText="This my hello3"
+      />
     </section>
   </div>
 </template>
 
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+
 <style scoped>
+.intro{
+  background-image: url('~assets/images/background.jpg');
+  background-position: center;
+  background-size: cover;
+  padding: 60px 0;
+}
 .intro h1{
   text-align: center;
+  margin: 0;
+  color: #fff;
+  font-size: 2em;
+  text-shadow: 1px 1px 2px black, 0 0 1px black;
 }
-  .post-preview {
-    border: 1px solid #ccc;
-    box-shadow: 0 2px 2px #ccc;
-    background-color: white;
-    width: 90%;
-  }
-  a {
-    text-decoration: none;
-    color: black;
-  }
-  @media (min-width: 850px) {
-    .post-preview {
-      width: 400px;
-      margin: 10px auto;
-      padding: 10px;
-    }
-  }
-  .post-thumbnail {
-    width: 100%;
-    height: 200px;
-    background-position: center;
-    background-size: cover;
-  }
+  
 
 </style>

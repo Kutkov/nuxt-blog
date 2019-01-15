d114eb4f3061d58427786b4c0e5bcfe085e1d5b3
Max <max.k194@gmail.com>
2019-01-15 18:58:26 +0300

    add auth, new-post, components navigation, post list

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="post-list">
+    <PostPreview 
+      id="1"
+      thumbnail="https://icdn.lenta.ru/images/2018/09/21/15/20180921152446974/pic_dff6499e5b87f34e7a8e4bcb50f8fb10.jpg"
+      title="Hello there"
+      previewText="This my hello"
+    />
+    <PostPreview 
+      id="2"
+      thumbnail="http://www.vsemirnyjbank.org/content/dam/photos/780x439/2017/jun-1/ru-city-780.jpg"
+      title="Hello there2"
+      previewText="This my hello3"
+    />
+    <PostPreview 
+      id="3"
+      thumbnail="https://forbes.kz/img/articles/99661f12e332c908c2a3a754d3f4942b-big.jpg"
+      title="Hello there3"
+      previewText="This my hello3"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+  .posts-list {
+    display: flex;
+    padding: 20px;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -3,34 +3,16 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview 
-        id="1"
-        thumbnail="https://icdn.lenta.ru/images/2018/09/21/15/20180921152446974/pic_dff6499e5b87f34e7a8e4bcb50f8fb10.jpg"
-        title="Hello there"
-        previewText="This my hello"
-      />
-      <PostPreview 
-        id="2"
-        thumbnail="http://www.vsemirnyjbank.org/content/dam/photos/780x439/2017/jun-1/ru-city-780.jpg"
-        title="Hello there2"
-        previewText="This my hello3"
-      />
-      <PostPreview 
-        id="3"
-        thumbnail="https://forbes.kz/img/articles/99661f12e332c908c2a3a754d3f4942b-big.jpg"
-        title="Hello there3"
-        previewText="This my hello3"
-      />
-    </section>
+    <PostList />
+
   </div>
 </template>
 
 <script>
-import PostPreview from '@/components/Posts/PostPreview'
+import PostList from '@/components/Posts/PostList'
 export default {
   components: {
-    PostPreview
+    PostList
   }
 }
 </script>
